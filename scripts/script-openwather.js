a1d74191e7bb098c4cabbf30f08bc7bcec5f3984
Matheus Bezerra Lima <matheusbeducation@gmail.com>
2024-10-01 12:27:47 -0300

    update

--- scripts/script-openwather.js
+++ scripts/script-openwather.js
@@ -1,18 +1,36 @@
-const apiKey = '20fba1ad64c719dbf6e527049f292875';
-const city = 'Cajamar';
+const toggleTheme = () => {
+    const body = document.querySelector('.backEffect');
+    
+    // Alterna a classe 'dark-theme' no body
+    if (body.classList.contains('light-theme')) {
+        body.classList.remove('light-theme');  // Remove tema escuro
+    } else {
+        body.classList.add('light-theme');  // Adiciona tema escuro
+    }
+}
 
 
+const alterMode = (object) => {
+    let sunImage = 'url(../imgs/sol.png)' 
+    let moonImage = 'url(../imgs/lua.png)'
+    let dayImage = 'url(../imgs/dia.jpg)'
+    let nightImage= 'url(../imgs/night.jpg)'
 
+    let currentMode = object.getAttribute("modo-atual")
 
-const getData = async() => {
+    if(currentMode == "lua"){
+        object.style.backgroundImage = sunImage
+        object.style.transform = 'translateX(80px)'
+        object.parentElement.style.backgroundImage = dayImage;  
 
-    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+        object.setAttribute("modo-atual", "sol"); 
 
-    const response = await fetch(apiWeatherURL);
-    const data = await response.json();
+    }else{
+        object.style.backgroundImage = moonImage
+        object.style.transform = 'translateX(0)';
+        object.parentElement.style.backgroundImage = nightImage;  
+        object.setAttribute("modo-atual", "lua"); 
+    }
 
-    console.log(data);
-
-};
-
-getData();
+    toggleTheme()
+}
